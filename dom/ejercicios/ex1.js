dec923c0b1938719dc62b7243c23431ca36b8ba8
Martin Ciro <52991306+MartinCiro@users.noreply.github.com>
2023-04-27 15:02:14 -0500

    Update ex1.js

--- dom/ejercicios/ex1.js
+++ dom/ejercicios/ex1.js
@@ -1,16 +1,20 @@
 console.log("Este es ex1.js")
 
-//fecha y año actual
-fecha=new Date()
-const Actual = fecha.getFullYear();
-names=("martin")
+function enviarAlerta(){
+    let dato = document.getElementsByName('texto')[0].value;
+    let dato1 = document.getElementsByName('texto')[1].value;
+    alert(dato + dato1);
+  }
 
+//fecha y año actual
+fecha = new Date()
+const Actual = fecha.getFullYear();
+nam = document.getElementsByName('texto')[0].value;
 naci=2001;
 age=Actual-naci
 
 if(age<0){
     alert("Error, año de nacimiento mayor a la fecha actual")
 }else{
-    document.getElementById("blue_box").innerHTML=`Hola, mi nombre es ${names} tengo ${age} años y estoy aprendiendo JavaScrip`
+    document.getElementById("blue_box").innerHTML=`Hola, mi nombre es ${nam} tengo ${age} años y estoy aprendiendo JavaScript`
 }
-
