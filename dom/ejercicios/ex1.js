8f79aff20b12f256a0eae9a9d3b1d540cb4c6c6b
Martin Ciro <52991306+MartinCiro@users.noreply.github.com>
2023-04-28 15:24:37 -0500

    Add files via upload

--- dom/ejercicios/ex1.js
+++ dom/ejercicios/ex1.js
@@ -1,20 +1,26 @@
 console.log("Este es ex1.js")
 
-function enviarAlerta(){
-    let dato = document.getElementsByName('texto')[0].value;
-    let dato1 = document.getElementsByName('texto')[1].value;
-    alert(dato + dato1);
-  }
-
-//fecha y año actual
-fecha = new Date()
-const Actual = fecha.getFullYear();
-nam = document.getElementsByName('texto')[0].value;
-naci=2001;
-age=Actual-naci
-
-if(age<0){
-    alert("Error, año de nacimiento mayor a la fecha actual")
-}else{
-    document.getElementById("blue_box").innerHTML=`Hola, mi nombre es ${nam} tengo ${age} años y estoy aprendiendo JavaScript`
+function reloa(){
+    window.location.reload(1);
 }
+
+function enviarAlerta() {
+    //fecha y año actual
+    var fecha = new Date();
+    const Actual = fecha.getFullYear();
+    let nam = document.getElementsByName('texto')[0].value;
+    let apellido = document.getElementsByName('texto')[1].value;
+
+    //valor fecha yy-mm-dd
+    let fech = document.getElementsByName('texto')[2].value;
+    age = Actual - fech
+    
+    if (age <= 0) {
+        alert("Error, año de nacimiento mayor o igual a la fecha actual")
+    } else {
+        document.writeln(document.getElementById("student_message").innerHTML = `Hola, mi nombre es ${nam} ${apellido} tengo ${age} años y estoy aprendiendo JavaScript`);
+        document.writeln("<br><input type='submit' value='Volver al inicio' onclick='reloa();'></input>")
+    }
+}
+
+
